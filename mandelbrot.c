d9ba37a57977743e9ad1d78c4578421e1202543d
Renzo Rossato Battisti <renzo.battisti1@gmail.com>
2024-08-11 23:18:51 -0300

    image is now malloced, names are cleaner

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,21 +1,21 @@
 #include <complex.h>
 #include <tgmath.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
  
 // Defining the size of the screen.
 #define Y 1080
 #define X 1920
  
-typedef struct out_pixel{
+typedef struct pixel{
   int x;
   int y;
   int color[3];
-}out_pixel_t;
+}pixel_t;
 
 // Recursive function to provide the iterative every 100th
 // f^n (0) for every pixel on the screen.
-out_pixel_t Mandle(double _Complex c,
+pixel_t Mandle(double _Complex c,
            double _Complex t,
            int counter)
 {
@@ -23,9 +23,9 @@ out_pixel_t Mandle(double _Complex c,
     // To eliminate out of bound values.
     if (cabs(t) > 4) {
         int color = 128 - 128 * cabs(t) / cabs(c);
-        out_pixel_t pixel = {creal(c) * X / 4 + X / 2,
-                             cimag(c) * Y / 2 + Y / 2,
-                             {color, color, color}};
+        pixel_t pixel = {creal(c) * X / 4 + X / 2,
+                         cimag(c) * Y / 2 + Y / 2,
+                         {color, color, color}};
         return pixel;
     }
  
@@ -36,9 +36,9 @@ out_pixel_t Mandle(double _Complex c,
     // more processing time.
     if (counter == 100) {
         int color = 255 * (cabs((t * t)) / cabs((t - c) * c));
-        out_pixel_t pixel = {creal(c) * X / 4 + X / 2,
-                             cimag(c) * Y / 2 + Y / 2,
-                             {color, 0, 0}};
+        pixel_t pixel = {creal(c) * X / 4 + X / 2,
+                         cimag(c) * Y / 2 + Y / 2,
+                         {color, 0, 0}};
         return pixel;
     }
  
@@ -47,27 +47,56 @@ out_pixel_t Mandle(double _Complex c,
     return Mandle(c, cpow(t, 2) + c, counter + 1);
 }
  
-void MandleSet(int out_image[X][Y][3])
+void MandleSet(int*** image)
 {
-    int count = 0;
-    
     // Calling Mandle function for every
     // point on the screen.
     for (double x = -2; x < 2; x += 0.0015) {
         for (double y = -1; y < 1; y += 0.0015) {
             double _Complex temp = x + y * _Complex_I;
-            out_pixel_t temp_pixel = Mandle(temp, 0, 0);
-            out_image[temp_pixel.x][temp_pixel.y][0] = temp_pixel.color[0];
-            out_image[temp_pixel.x][temp_pixel.y][1] = temp_pixel.color[1];
-            out_image[temp_pixel.x][temp_pixel.y][2] = temp_pixel.color[2];
-            count++;
+            pixel_t pixel = Mandle(temp, 0, 0);
+            image[pixel.x][pixel.y][0] = pixel.color[0];
+            image[pixel.x][pixel.y][1] = pixel.color[1];
+            image[pixel.x][pixel.y][2] = pixel.color[2];
         }
     }
 }
- 
+
+int*** malloc_image(){
+    int*** image = (int***)malloc(sizeof(int**) * X);
+
+    for(int i = 0; i < X; i++){
+        image[i] = (int**)malloc(sizeof(int*) * Y);
+    }
+
+    for(int i = 0; i < X; i++){
+        for(int j = 0; j < Y; j++){
+            image[i][j] = (int*)malloc(sizeof(int) * 3);
+        }
+    }
+
+    return image;
+}
+
+void free_image(int*** image){
+
+    for(int i = 0; i < X; i++){
+        for(int j = 0; j < Y; j++){
+            free(image[i][j]);
+        }
+    }
+    for(int i = 0; i < X; i++){
+        free(image[i]);
+    }
+
+    free(image);
+}
+
 int main()
 {
-    int image[X][Y][3];
+    int*** image = malloc_image();
     MandleSet(image);
+
+    free_image(image);
     return 0;
 }
