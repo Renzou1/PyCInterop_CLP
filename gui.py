aed78ab93733057f2318ce9b9ea5e0fc938da426
Renzo Rossato Battisti <renzo.battisti1@gmail.com>
2024-08-14 19:18:52 -0300

    cleaner code, added free_image to python

--- gui.py
+++ gui.py
@@ -4,30 +4,18 @@ from PIL import Image, ImageColor
 shared_library_path = "./mandelbrot.so"
 
 my_funcs = CDLL(shared_library_path)
-# my_funcs.e.restype = POINTER(c_int)
 
 my_funcs.build_image()
 
-
-
-# pointer = my_funcs.e()
-# print(pointer)
-
-# #res = [[[pointer[i * 1080 * 3 + j * 3 + k] for k in range(3)] for j in range(1080)] for i in range(1920)]
-
-# res = [pointer[i] for i in range(1920 * 1080 * 3)]
-# a = []
 im = Image.new('RGB', (1920, 1080), "black") # starts as a black image
 pixels = im.load() #creates pixel map, whatever that is
 
 for i in range(im.size[0]): #for every col
    for j in range(im.size[1]): #for every row
-        r = my_funcs.r(i, j, 0)
-        g = my_funcs.r(i, j, 1)
-        b = my_funcs.r(i, j, 2)
+        r = my_funcs.get_pixel(i, j, 0)
+        g = my_funcs.get_pixel(i, j, 1)
+        b = my_funcs.get_pixel(i, j, 2)
         pixels[i, j] = (r, g, b)
-        #    a.append()
 
-#im.show()
 im.save('mandelbrot2.png')
-# print(res[0:50])
+my_funcs.free_image()
